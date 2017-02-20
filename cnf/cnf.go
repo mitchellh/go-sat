b18fff39ee52d899f64b614f1005ef8aec46cf9f
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2017-02-20 15:17:30 -0800

    cnf: package to store structures/operations on CNF formulas

---
+++ cnf/cnf.go
@@ -0,0 +1,76 @@
+// Package cnf contains structures and operations for boolean formulas
+// in conjunctive normal form (CNF).
+package cnf
+
+// Formula represents a Boolean formula in conjunctive normal form (CNF).
+type Formula []Clause
+
+// Clause is a disjunction of literals.
+type Clause []Literal
+
+// Literal is a single literal (such as X, Y, etc.).
+//
+// The value of the literal is a integer to uniquely identify this literal.
+// Two literals with the same |value| (absolute value of their value) are
+// the same literal. A negative literal indicates a negated literal.
+type Literal int
+
+// NewFormulaFromInts is a helper to turn [][]int into a Formula where
+// the input is a slice of clauses and each int is a literal.
+func NewFormulaFromInts(raw [][]int) Formula {
+	cs := make([]Clause, len(raw))
+	for i, v := range raw {
+		cs[i] = make([]Literal, len(v))
+		for j, x := range v {
+			cs[i][j] = Literal(x)
+		}
+	}
+
+	return Formula(cs)
+}
+
+func (f Formula) Ints() [][]int {
+	result := make([][]int, len(f))
+	for i, c := range f {
+		result[i] = make([]int, len(c))
+		for j, l := range c {
+			result[i][j] = int(l)
+		}
+	}
+
+	return result
+}
+
+// Negate the formula by negating all literals in all clauses.
+func (f Formula) Negate() Formula {
+	result := make([]Clause, len(f))
+	for i, c := range f {
+		resultC := make([]Literal, len(c))
+		for j, l := range c {
+			resultC[j] = Literal(-int(l))
+		}
+
+		result[i] = Clause(resultC)
+	}
+
+	return Formula(result)
+}
+
+func (f Formula) Vars() map[Literal]struct{} {
+	set := make(map[Literal]struct{})
+	for _, c := range f {
+		for _, l := range c {
+			if l < 0 {
+				l = -l
+			}
+
+			set[l] = struct{}{}
+		}
+	}
+
+	return set
+}
+
+func (l Literal) Negate() Literal {
+	return Literal(int(l) * -1)
+}
---
+++ cnf/cnf_test.go
@@ -0,0 +1,36 @@
+package cnf
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestFormulaNegate(t *testing.T) {
+	cases := []struct {
+		Input  [][]int
+		Output [][]int
+	}{
+		{
+			[][]int{
+				[]int{1},
+				[]int{-3, 4},
+			},
+			[][]int{
+				[]int{-1},
+				[]int{3, -4},
+			},
+		},
+	}
+
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			actual := NewFormulaFromInts(tc.Input).Negate()
+			expected := NewFormulaFromInts(tc.Output)
+
+			if !reflect.DeepEqual(actual, expected) {
+				t.Fatalf("bad: %#v", actual)
+			}
+		})
+	}
+}
